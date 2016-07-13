70fc8caa423764a2f7290d9157f6a732508411fb
dddsnn <marc.lehmann@gmx.de>
2016-07-13 18:57:12 +0000

    Add retrying

--- src/discord_connection.rs
+++ src/discord_connection.rs
@@ -1,18 +1,22 @@
 extern crate discord;
 
+use std;
 use discord::model::Event;
 use discord::model::ChannelId;
 use discord::model::CurrentUser;
 use discord::model::Message;
 
+
+const MAX_RETRIES: u32 = 5;
+
 pub trait DiscordConnection {
-    fn recv_event(&mut self) -> Result<Event, &str>;
+    fn recv_event(&mut self) -> Result<Event, String>;
     fn send_message(&self,
                     channel: &ChannelId,
                     text: &str,
                     nonce: &str,
                     tts: bool)
-                    -> Result<Message, &str>;
+                    -> Result<Message, String>;
 }
 
 pub struct BotConnection {
@@ -34,28 +38,54 @@ impl BotConnection {
         },
          me)
     }
-}
 
-
-impl DiscordConnection for BotConnection {
-    fn recv_event(&mut self) -> Result<Event, &str> {
-        match self.conn.recv_event() {
-            // TODO error handling
-            Err(_) => Err("error"),
-            Ok(e) => Ok(e),
-        }
+    fn retry<R>(f: &mut FnMut() -> Result<R, discord::Error>) -> Result<R, String> {
+        Self::retry_n(f, MAX_RETRIES, "Maximum number of retries exceeded.")
     }
 
+    fn retry_n<R>(f: &mut FnMut() -> Result<R, discord::Error>,
+                  tries: u32,
+                  last_err_msg: &str)
+                  -> Result<R, String> {
+        if tries == 0 {
+            return Err(last_err_msg.to_owned());
+        }
+        match f() {
+            Ok(r) => Ok(r),
+            Err(err) => {
+                match err {
+                    discord::Error::RateLimited(millis) => {
+                        // Rate limited, sleep the prescribed amount of ms. Don't decrese the number
+                        // of tries because this can always be fixed.
+                        std::thread::sleep(std::time::Duration::from_millis(millis));
+                        Self::retry_n(f, tries, last_err_msg)
+                    }
+                    _ => {
+                        // Some other error, wait a second and hope the cause goes away.
+                        // TODO maybe handle other specific cases?
+                        std::thread::sleep(std::time::Duration::from_secs(1));
+                        Self::retry_n(f, tries - 1, &format!("{}", err))
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl DiscordConnection for BotConnection {
+    /// Returns an error message on error.
+    fn recv_event(&mut self) -> Result<Event, String> {
+        // TODO do i have to use a move closure? no way to simply pass a function?
+        Self::retry(&mut move || self.conn.recv_event())
+    }
+
+    /// Returns an error message on error.
     fn send_message(&self,
                     channel: &ChannelId,
                     text: &str,
                     nonce: &str,
                     tts: bool)
-                    -> Result<Message, &str> {
-        // TODO error handling
-        match self.discord.send_message(channel, text, nonce, tts) {
-            Err(_) => Err("error"),
-            Ok(msg) => Ok(msg),
-        }
+                    -> Result<Message, String> {
+        Self::retry(&mut move || self.discord.send_message(channel, text, nonce, tts))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     if let Some(t) = std::env::args().nth(1) {
         token = t;
     } else {
+        // TODO log, don't print
         println!("Pass the bot token as an argument.");
         std::process::exit(1);
     }
@@ -46,17 +47,14 @@ impl ShBot<BotConnection> {
     fn run(mut self) {
         loop {
             self.handle_event();
-            // Wait a bit because of the rate limit.
-            // TODO do smarter retrying
-            std::thread::sleep(std::time::Duration::from_secs(1));
         }
     }
 
     fn handle_event(&mut self) {
-        // TODO remove return type (only used to limit number of loops)
         match self.discord.recv_event() {
-            Err(_) => {
-                println!("error receiving event");
+            Err(msg) => {
+                // TODO log, don't print
+                println!("Error receiving event: {}", msg);
             }
             Ok(Event::MessageCreate(msg)) => {
                 if msg.author.id == self.me.id {
@@ -70,7 +68,9 @@ impl ShBot<BotConnection> {
                 };
                 self.handle_request(req);
             }
-            _ => {}
+            _ => {
+                // Event we don't care about.
+            }
         }
     }
 
@@ -103,25 +103,31 @@ impl ShBot<BotConnection> {
 
     fn handle_echo(&self, req: Request, options: &str) {
         let reply = req.author.name.clone() + " wants me to echo \"" + options + "\".";
-        self.discord
-            .send_message(&req.channel_id, &reply, "", false)
-            .expect("failed to send msg");
+        if let Err(msg) = self.discord
+            .send_message(&req.channel_id, &reply, "", false) {
+            // TODO log, don't print
+            println!("Failed to send message: {}", msg);
+        }
     }
 
     fn handle_help(&self, req: Request, options: &str) {
         // TODO
         let reply = "This is an unhelpful help text. There'll be a better one, I promise.";
-        self.discord
-            .send_message(&req.channel_id, &reply, "", false)
-            .expect("failed to send msg");
+        if let Err(msg) = self.discord
+            .send_message(&req.channel_id, &reply, "", false) {
+            // TODO log, don't print
+            println!("Failed to send message: {}", msg);
+        }
     }
 
     fn handle_unknown(&self, req: Request, unknown_command: &str) {
         let reply = "\"".to_owned() + unknown_command +
                     "\" is not a valid command. Type \"help\" to find out what is.";
-        self.discord
-            .send_message(&req.channel_id, &reply, "", false)
-            .expect("failed to send msg");
+        if let Err(msg) = self.discord
+            .send_message(&req.channel_id, &reply, "", false) {
+            // TODO log, don't print
+            println!("Failed to send message: {}", msg);
+        }
     }
 
     fn handle_shutdown(&self) {
